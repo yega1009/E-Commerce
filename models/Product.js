9d14cfc17d1d36ef228ea58b71c1c79a267232dc
Yega Cho <yega1009@gmail.com>
2023-10-10 17:38:39 -0400

    added comments for product.js

--- models/Product.js
+++ models/Product.js
@@ -6,19 +6,22 @@ const sequelize = require("../config/connection");
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Set up fields and rules for Product model
 Product.init(
   {
+    // Define primary key ID
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Define product_name field
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Defining price field with validation for decimal values
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
@@ -26,6 +29,7 @@ Product.init(
         isDecimal: true,
       },
     },
+    // Define stock field with a default value of 10 and numeric validation
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -34,14 +38,15 @@ Product.init(
         isNumeric: true,
       },
     },
+    // Define category_id field with foreign key that refers to the category model
     category_id: {
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
-        model: 'category',
-        key: 'id',
+        model: "category",
+        key: "id",
       },
-    }
+    },
   },
   {
     sequelize,
