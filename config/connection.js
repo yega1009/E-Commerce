32df53c947ad26a5b151f9cc74b075f339179963
Yega Cho <yega1009@gmail.com>
2023-10-10 17:52:39 -0400

    added comments for connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
-require('dotenv').config();
+// Import  'dotenv' to access variables in .env file
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+// Import 'sequelize'
+const Sequelize = require("sequelize");
 
+// Create a new connection to the database.
+// If JAWSDB_URL environment variable is available (when deployed on Heroku), use it to connect.
+// Otherwise, connect to the local MySQL database.
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+  ? // If application is deployed (ex. on Heroku) and JAWSDB_URL is available,
+    // connect to the JawsDB database using the URL stored in the JAWSDB_URL environment variable
+    new Sequelize(process.env.JAWSDB_URL)
+  : // Otherwise, connect to the local MySQL database using the database name, username, and password stored
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 
+// Export the 'sequelize' connection instance to be used in the application
 module.exports = sequelize;
