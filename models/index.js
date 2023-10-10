23b8402bf26532667352767a505a26b8425ab028
Yega Cho <yega1009@gmail.com>
2023-10-10 17:46:29 -0400

    added comments for index.js

--- models/index.js
+++ models/index.js
@@ -4,10 +4,10 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category (i.e., Each Product is associated with one Category)
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE', // if a category is deleted, its products are also deleted
 });
 
 // Categories have many Products
@@ -16,6 +16,7 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+// Many-to-many relationship between Product and Tag
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
