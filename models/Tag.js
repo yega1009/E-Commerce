d05e3085fca20ec72064c2f6b6e7c236379559a7
Yega Cho <yega1009@gmail.com>
2023-10-10 17:43:01 -0400

    added comments for tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -6,12 +6,14 @@ class Tag extends Model {}
 
 Tag.init(
   {
+    // Define primary key ID
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Define tag_name field
     tag_name: {
       type: DataTypes.STRING,
     },
