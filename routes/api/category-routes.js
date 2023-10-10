05d4547c55a0a4ffe0605a672914b403fdefd495
Yega Cho <yega1009@gmail.com>
2023-10-10 18:18:35 -0400

    completed put route for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  // Update a category by its ID
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    
+    if (!updatedCategory) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Category updated successfully!" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
