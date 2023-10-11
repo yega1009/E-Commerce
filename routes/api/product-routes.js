80721bbe23d194103f374e7e05fc1ab895a1dd86
Yega Cho <yega1009@gmail.com>
2023-10-11 00:56:46 -0400

    completed delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,7 +50,7 @@ router.post("/", (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -113,8 +113,20 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedRows = await Product.destroy({ where: { id: req.params.id } });
+
+    if (deletedRows === 0) {
+      res.status(404).json({ message: "Product not found!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Product deleted successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
