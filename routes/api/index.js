61ac31358506a0fa16536699c8c4b1bec4722fc1
Yega Cho <yega1009@gmail.com>
2023-10-11 17:09:09 -0400

    added comments to api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
+// Import the required modules and router from Express
 const router = require('express').Router();
+
+// Import the route files
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// Assign the imported routes to their paths
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
