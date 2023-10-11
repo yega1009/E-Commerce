bd868110e1037b639a28aeb899c5460cd0526f5a
Yega Cho <yega1009@gmail.com>
2023-10-10 23:31:55 -0400

    completed delete route for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,7 +53,7 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
-    
+
     if (!updatedCategory) {
       res.status(404).json({ message: "No category found with this id!" });
       return;
@@ -65,8 +65,24 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a category by its id value
+  try {
+    const categoryToDelete = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryToDelete) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
