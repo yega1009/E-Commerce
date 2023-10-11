60fb7a2a0b598191b3e1d25d95ba2d739203d940
Yega Cho <yega1009@gmail.com>
2023-10-11 17:02:53 -0400

    added comments to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,12 +3,12 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
-// get all products
+// GET route to fetch all products including their categories and tags
 router.get("/", async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
+    // Sequelize's findAll method to get all products with included Category and Tag data
     const products = await Product.findAll({
+      // The tags associated with each product, as per the ProductTag table, are fetched from the Tag table.
       include: [Category, { model: Tag, through: ProductTag }],
     });
     res.status(200).json(products);
@@ -17,10 +17,8 @@ router.get("/", async (req, res) => {
   }
 });
 
-// get one product
+// GET route to fetch a product by its ID, including its category and tags
 router.get("/:id", async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const product = await Product.findByPk(req.params.id, {
       include: [Category, { model: Tag, through: ProductTag }],
@@ -37,19 +35,11 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// create new product
+// POST route to create a new product with its tags
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      // If there are tags for the product, create entries in the ProductTag model
       if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -57,9 +47,10 @@ router.post("/", (req, res) => {
             tag_id,
           };
         });
+        // Bulk create entries in the ProductTag model
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
+      
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -69,9 +60,8 @@ router.post("/", (req, res) => {
     });
 });
 
-// update product
+// PUT route to update the product data with its tags
 router.put("/:id", (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -82,7 +72,7 @@ router.put("/:id", (req, res) => {
         ProductTag.findAll({
           where: { product_id: req.params.id },
         }).then((productTags) => {
-          // create filtered list of new tag_ids
+          // Filter out new tags and removing old tag associations
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
             .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -93,11 +83,12 @@ router.put("/:id", (req, res) => {
               };
             });
 
-          // figure out which ones to remove
+          // Identify tags to remove
           const productTagsToRemove = productTags
             .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
             .map(({ id }) => id);
-          // run both actions
+          
+          // Add and remove operations
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
@@ -108,14 +99,14 @@ router.put("/:id", (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
+// DELETE route to remove a product by its ID
 router.delete("/:id", async (req, res) => {
-  // delete one product by its `id` value
   try {
+    // Sequelize's destroy method to delete the product
     const deletedRows = await Product.destroy({ where: { id: req.params.id } });
 
     if (deletedRows === 0) {
