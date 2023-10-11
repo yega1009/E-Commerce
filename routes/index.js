5d027c9a401f8f26a08e3bb9ce4bae968bce43a2
Yega Cho <yega1009@gmail.com>
2023-10-11 17:10:06 -0400

    added comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+// Import the Express router and API routes
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Assign the API routes to the '/api' path
 router.use('/api', apiRoutes);
 
+// Handle requests to undefined routes
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
